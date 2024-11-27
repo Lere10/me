e22be06a4c035e62c9ee95062490c3b6c83f4fa4
Lere <leredsb10@gmail.com>
2024-11-27 03:00:35 -0300

    portfolioFstComit

--- Me/src/components/Portfolio/PortfolioHeader/PortfolioHeader.jsx
+++ Me/src/components/Portfolio/PortfolioHeader/PortfolioHeader.jsx
@@ -6,35 +6,44 @@ export default function PortfolioHeader() {
   const [devActive, setDevActive] = useState();
   const [desActive, setDesActive] = useState();
 
-  async function handleClick() {
-    const activeHeaderClass = "portfolio__header_active";
+  const activeHeaderClass = "portfolio__header_active";
+  const offHeaderClass = "portfolio__header_off";
+
+  async function handleClickDev() {
     const devHeader = document.querySelector(".portfolio__header-dev");
     const desHeader = document.querySelector(".portfolio__header-design");
+    devHeader.classList.add(activeHeaderClass);
+    devHeader.classList.remove(offHeaderClass);
+    desHeader.classList.remove(activeHeaderClass);
+    desHeader.classList.add(offHeaderClass);
+    setDevActive(true);
+    setDesActive(false);
+  }
 
-    devHeader.addEventListener("click", () => {
-      devHeader.classList.add(activeHeaderClass);
-      desHeader.classList.remove(activeHeaderClass);
-      setDevActive(true);
-      setDesActive(false);
-    });
-    desHeader.addEventListener("click", () => {
-      desHeader.classList.add(activeHeaderClass);
-      devHeader.classList.remove(activeHeaderClass);
-      setDevActive(false);
-      setDesActive(true);
-    });
+  async function handleClickDes() {
+    const devHeader = document.querySelector(".portfolio__header-dev");
+    const desHeader = document.querySelector(".portfolio__header-design");
+    desHeader.classList.add(activeHeaderClass);
+    desHeader.classList.remove(offHeaderClass);
+    devHeader.classList.remove(activeHeaderClass);
+    devHeader.classList.add(offHeaderClass);
+    setDevActive(false);
+    setDesActive(true);
   }
 
   return (
     <div className="portfolio__header">
       <div
-        onClick={handleClick}
+        onClick={handleClickDev}
         className="portfolio__header-dev portfolio__header_active"
       >
         <p className="portfolio__pretitle">Portfólio</p>
         <h2 className="portfolio__title">Desenvolvimento Web</h2>
       </div>
-      <div onClick={handleClick} className="portfolio__header-design">
+      <div
+        onClick={handleClickDes}
+        className="portfolio__header-design portfolio__header_off"
+      >
         <p className="portfolio__pretitle">Portfólio</p>
         <h2 className="portfolio__title">Web Design</h2>
       </div>
