84611f5d1e782cbf1ff207581a5ba463b07d08e4
Lere <leredsb10@gmail.com>
2025-01-01 18:11:47 -0300

    popup css done

--- Me/src/components/Portfolio/Popup/Popup.jsx
+++ Me/src/components/Portfolio/Popup/Popup.jsx
@@ -20,16 +20,24 @@ export default function Popup(props) {
             ) : (
               ""
             )}
-            {img ? <img className="popup__image" src={img} alt={title} /> : ""}
-            {title ? <h3 className="popup__title">{title}</h3> : ""}
-            {desc ? <p className="popup__desc">{desc}</p> : ""}
-            {link ? (
-              <a className="popup__link" href={link} target="_blank">
-                Veja o projeto inteiro aqui
-              </a>
+            {img ? (
+              <div className="popup__image-box">
+                <img className="popup__image" src={img} alt={title} />
+              </div>
             ) : (
               ""
             )}
+            <div className="popup__content-text">
+              {title ? <h3 className="popup__title">{title}</h3> : ""}
+              {desc ? <p className="popup__desc">{desc}</p> : ""}
+              {link ? (
+                <a className="popup__link" href={link} target="_blank">
+                  Veja o projeto inteiro aqui
+                </a>
+              ) : (
+                ""
+              )}
+            </div>
           </div>
         </section>
       ) : (
